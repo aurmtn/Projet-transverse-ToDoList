774837f561a4a6410b4bf63b6088c3e9adb198f5
alixxila <alix.techoffice@gmail.com>
2021-03-26 11:25:43 +0100

    Page de Login/Inscription fonctionnel index2.php

--- index2.php
+++ index2.php
@@ -2,7 +2,8 @@
 
 require 'db-config.php';
 
-if($_POST['login']){
+if($_POST['login']) 
+{
 
 $user = $_POST['username'];
 $password = $_POST['password'];
@@ -14,10 +15,23 @@ $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
 $stmt = $PDO->prepare($sql);
 
 $stmt->execute(['username' => $user, 'password' => $password]);
-
-else{
-    ($_POST[''])
 }
+else
+{
+      
+        $user = $_POST['username'];
+        $password = $_POST['password'];
+        $mail = $_POST['email'];
+
+        $PDO = new PDO('mysql:host=localhost;dbname=to_do_list',$DB_USER,$DB_PASS,$options);
+
+        $sql = "INSERT INTO users (username, password, email) VALUES (:username, :password, :email)";
+
+        $stmt = $PDO->prepare($sql);
+
+        $stmt->execute(['username' => $user, 'password' => $password, 'email' => $mail]);
+        
+
 }
 
 
