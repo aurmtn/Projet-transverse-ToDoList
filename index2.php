767d027d47e76132a2da2f664eba08da177ef39f
alixxila <alix.techoffice@gmail.com>
2021-04-08 12:43:34 +0200

    test

---
+++ index2.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'db-config.php';
+
+if($_POST['login']) 
+{
+
+$user = $_POST['username'];
+$password = $_POST['password'];
+
+$PDO = new PDO('mysql:host=localhost;dbname=to_do_list',$DB_USER,$DB_PASS,$options);
+
+$sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+
+$stmt = $PDO->prepare($sql);
+
+$stmt->execute(['username' => $user, 'password' => $password]);
+}
+else
+{
+      
+        $user = $_POST['username'];
+        $password = $_POST['password'];
+        $mail = $_POST['email'];
+
+        $PDO = new PDO('mysql:host=localhost;dbname=to_do_list',$DB_USER,$DB_PASS,$options);
+
+        $sql = "INSERT INTO users (username, password, email) VALUES (:username, :password, :email)";
+
+        $stmt = $PDO->prepare($sql);
+
+        $stmt->execute(['username' => $user, 'password' => $password, 'email' => $mail]);
+        
+
+}
+
+
+header("Location: http://localhost:80/TODODO/ToDoGit/Projet-transverse-ToDoList/connexion inscription/home.html");
+
+?>
